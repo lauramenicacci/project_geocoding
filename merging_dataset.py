516ccad3595d41026fad1b8a1b7cc5bddc563227
zazzooo <lorenzo.gini1998@gmail.com>
2021-10-12 13:58:20 +0200

    Just correct some spelling mistake and merged all the datasets

---
+++ merging_dataset.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+list = ['dataset_india_export','dataset_india_import','dataset_brazil_export','dataset_brazil_import',
+        'dataset_china_export','dataset_china_import','dataset_russia_export','dataset_russia_import',
+        'dataset_us_export','dataset_us_import','dataset_southafr_export','dataset_southafr_import']
+
+df_list = []
+for i in list:
+    print(i)
+    PATH = 'dataset_project_DSA\\'+ i +'\\DS-1262527_1_Data.csv'
+    data = pd.read_csv(PATH)
+    df_list.append(data)
+
+final_dataset = pd.concat(df_list)
+final_dataset.to_csv('complete_dataset.csv')
