269b0a27bcde199b4a808ccbf716591e8ca90810
zazzooo <lorenzo.gini1998@gmail.com>
2021-10-12 14:00:11 +0200

    forgot soe comments

--- merging_dataset.py
+++ merging_dataset.py
@@ -7,6 +7,7 @@ list = ['dataset_india_export','dataset_india_import','dataset_brazil_export','d
 df_list = []
 for i in list:
     print(i)
+    # the next line of code don't work on IOS, substitute // with \
     PATH = 'dataset_project_DSA\\'+ i +'\\DS-1262527_1_Data.csv'
     data = pd.read_csv(PATH)
     df_list.append(data)
