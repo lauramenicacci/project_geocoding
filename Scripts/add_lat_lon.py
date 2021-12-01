793bf6539eef360d298f076c90d8e34a9eb684d3
zazzooo <lorenzo.gini1998@gmail.com>
2021-12-01 12:54:47 +0100

    add function instead of scripts

---
+++ Scripts/add_lat_lon.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+def clean_df(df):
+    drop_df = df.loc[df['REPORTER'] == 'Czechia']
+    df = df.drop(drop_df.index)
+    df['REPORTER'] = df['REPORTER'].replace({'Belgium (incl. Luxembourg \'LU\' -> 1998)':'Belgium'})
+    df['REPORTER'] = df['REPORTER'].replace({'Germany (incl. German Democratic Republic \'DD\' from 1991)':'Germany'})
+    df['REPORTER'] = df['REPORTER'].replace({'Ireland (Eire)':'Ireland'})
+    df['REPORTER'] = df['REPORTER'].replace({'Spain (incl. Canary Islands \'XB\' from 1997)':'Spain'})
+    df['REPORTER'] = df['REPORTER'].replace({'France (incl. Saint Barth�lemy \'BL\' -> 2012; incl. French Guiana \'GF\', Guadeloupe \'GP\', Martinique \'MQ\', R�union \'RE\' from 1997; incl. Mayotte \'YT\' from 2014)':'France'})
+    df['REPORTER'] = df['REPORTER'].replace({'Italy (incl. San Marino \'SM\' -> 1993)':'Italy'})
+    return df
+
+def add_lat_lon(df, pos_countries):
+    d_lat = dict(zip(pos_countries['name'], pos_countries['latitude']))
+    d_long = dict(zip(pos_countries['name'], pos_countries['longitude']))
+    lat = []
+    long = []
+    for index, row in df.iterrows():
+        lat.append(d_lat[row['REPORTER']])
+        long.append(d_long[row['REPORTER']])
+    df['Latitude'] = lat
+    df['Longitude'] = long
+    return df
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('dataset_project_DSA/dataset_india_export/DS-1262527_1_Data.csv')
+    pos_countries = pd.read_csv('position_countries.csv')
+
+    print(add_lat_lon(clean_df(df), pos_countries))
---
+++ Scripts/clean_adn_merge_data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import add_lat_lon
+import os
+
+def merge_database(compleate_df, already_merged = []):
+    for dir in  os.listdir(str(os.getcwd()) + '/dataset_project_DSA'):
+        l = []
+        if dir[0:4] == 'data' and dir not in already_merged:
+            PATH = str(os.getcwd() + '/dataset_project_DSA/'+ dir +'/DS-1262527_1_Data.csv')
+            data = pd.read_csv(PATH)
+            data = add_lat_lon.clean_df(data)
+            data = add_lat_lon.add_lat_lon(data)
+            l.append(data)
+            already_merged.append(dir)
+    datas = pd.concat(l)
+    return pd.concat([compleate_df,datas]), already_merged
