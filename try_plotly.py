20c830faee9a31c69f15ab8d72c30f9fa4b095a3
zazzooo <lorenzo.gini1998@gmail.com>
2021-12-08 11:41:37 +0100

    working plot

--- try_plotly.py
+++ try_plotly.py
@@ -5,9 +5,13 @@ import plotly.express as px
 official_df = pd.read_csv('official_dataset.csv')
 lat = official_df['Latitude'].unique()
 lon = official_df['Longitude'].unique()
-official_df = official_df.loc[official_df['FLOW'] == 'IMPORT']
-official_df['Value'] = official_df['Value'].str.repalce(' ','')
-official_df['Value'] = pd.to_numeric(official_df['Value'], downcast="float")
+# official_df = official_df.loc[official_df['FLOW'] == 'IMPORT']
+# official_df['Value'] = official_df['Value'].str.strip()
+official_df['Value'] = official_df['Value'].str.replace(' ','').replace(':','0')
+# print(type(official_df['Value']))
+print(official_df['Value'])
+official_df['Value'] = official_df['Value'].astype(float)
+
 
 fig = px.scatter_geo(official_df,
                     lat = 'Latitude',
@@ -15,6 +19,37 @@ fig = px.scatter_geo(official_df,
                     animation_frame = 'PERIOD',
                     size_max = 55,
                     hover_name = 'REPORTER',
-                    size = 'Value')
+                    size = 'Value',
+                    color = 'PARTNER')
+fig.update_geos(fitbounds="locations", showcountries = True)
+fig.update_layout(
+        updatemenus=[
+        dict(
+            type = "buttons",
+            direction = "left",
+            buttons=list([
+                dict(
+                    args=[official_df['FLOW'], 'IMPORT'],
+                    label="Import",
+                    method="update"
+                ),
+                dict(
+                    args=[official_df['FLOW'], "EXPORT"],
+                    label="Export",
+                    method="update"
+                )
+            ]),
+            pad={"r": 10, "t": 10},
+            showactive=True,
+            x=0.11,
+            xanchor="left",
+            y=1.1,
+            yanchor="top"
+        ),
+    ]
+)
+
+
+
 
 fig.show()
